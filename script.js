67bddbdbfb6d43101be93372efd13d4a50fc0bf0
VitalGol <vital_gol@ukr.ne>
2019-01-14 15:29:59 +0200

    add addEventListener for click on arrows

--- script.js
+++ script.js
@@ -18,5 +18,35 @@ function startSlider() {
 	sliderImages[0].style.display = 'block';
 }
 
+// show prev image
+function slideLeft() {
+	reset();
+	sliderImages[current - 1].style.display = 'block';
+	current--;
+}
+
+// show next image
+function slideRight() {
+	reset();
+	sliderImages[current + 1].style.display = 'block';
+	current++;
+}
+
+// click left arrow
+arrowLeft.addEventListener('click', function () {
+	if (current === 0) {
+		current = sliderImages.length;
+	}
+	slideLeft();
+});
+
+// click right arrow
+arrowRight.addEventListener('click', function () {
+	if (current === sliderImages.length - 1) {
+		current = -1;
+	}
+	slideRight();
+});
+
 startSlider();
 }
